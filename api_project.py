f1cd93a9cc078960699ee3ea0c331fa3f6614fee
jpmllr89 <jpmllr89@gmail.com>
2019-03-26 11:12:52 -0400

    cleaned up the code to put variables first.  Made more readable

--- api_project.py
+++ api_project.py
@@ -2,18 +2,23 @@ import requests as r
 
 url = "https://icanhazdadjoke.com/search"
 
-while True:
-    term = input("What topic would you like to search?  ")
-    res = r.get(
+term = input("What topic would you like to search?  ")
+
+res = r.get(
     url, 
     headers={
         "Accept":"application/json",
         },
     params = {"term": term}
     )
-    json_data = res.json()
+
+json_data = res.json()
+
+
+while True:
     if json_data["total_jokes"] == 0:
         print("Enter another search query.")
+        break
     else:
         print("There are " + str(json_data["total_jokes"]) + " jokes")
         print("Here is one: \n" +json_data["results"][0]['joke'])
