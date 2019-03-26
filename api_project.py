df7991630d10142a8c6f7a7fcfddb0f023da57c8
jpmllr89 <jpmllr89@gmail.com>
2019-03-26 11:31:36 -0400

    encapsulated input section into a  function to be used over again in the while statement

--- api_project.py
+++ api_project.py
@@ -2,30 +2,31 @@ import requests as r
 import random as rand
 
 
-url = "https://icanhazdadjoke.com/search"
+def run_query():
+    url = "https://icanhazdadjoke.com/search"
 
-term = input("What topic would you like to search?  ")
+    term = input("What topic would you like to search?  ")
 
-res = r.get(
-    url, 
-    headers={
-        "Accept":"application/json",
-        },
-    params = {"term": term}
-    )
+    res = r.get(
+        url, 
+        headers={
+            "Accept":"application/json",
+            },
+        params = {"term": term}
+        )
 
-json_data = res.json()
-
+    json_data = res.json()
+    return json_data
 
 while True:
+    json_data = run_query()
     if json_data["total_jokes"] == 0:
         print("Enter another search query.")
-        break
     else:
         print("There are " + str(json_data["total_jokes"]) + " jokes")
         result_num = rand.choice(range(0, json_data["total_jokes"]))
         print("Here is one: \n" +json_data["results"][result_num]['joke'])
         another = input("Would you like to search another?(y/n)")
-        if another == "y":
+        if another == "n":
             break
 #print(json_data)
